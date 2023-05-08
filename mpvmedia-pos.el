32511e789aeb90ae02eec96d9d7d669547b35fde
psh <jumper047@gmail.com>
2023-05-08 04:32:05 +0300

    Add progress calculation, fixes

--- mpvmedia-pos.el
+++ mpvmedia-pos.el
@@ -1,19 +1,25 @@
-;;; mpvmedia-pos.el --- See position you stopped playing media in mpv -*- lexical-binding:t -*-
+;;; mpvmedia-pos.el --- Display position where you stopped playing media in mpv -*- lexical-binding:t -*-
+
 ;;; Code:
 
-(defvar mpvmedia-pos-watch-later-dir "/home/psh/.config/mpv/watch_later/")
+(defvar mpvmedia-pos-watch-later-dir "~/.config/mpv/watch_later/")
 (defvar mpvmedia-pos-prefer-with-path nil)
 
-(defvar mpvmedia-pos-format " [mpv stopped at: %s ]")
+(defvar mpvmedia-pos-format "Progress: %s%%")
+(defvar mpvmedia-pos-fallback-format "Stopped at: %s")
+(defvar mpvmedia-pos-completed-message "Completed")
+(defvar mpvmedia-pos-completed-threshold 0.95)
 (defvar mpvmedia-pos-watched-time-format "%h:%.2m:%.2s")
+(defvar mpvmedia-pos-mediainfo-command "mediainfo")
+(defvar mpvmedia-pos-mediainfo-args "--Inform=\"General;%Duration%\"")
 
-(defun mpvmedia-pos-watch-later-file (media-file)
+(defun mpvmedia-pos--get-watch-later-file (media-file)
   (let* (;; (file (expand-file-name file))
          (possible-filenames (list media-file (file-name-nondirectory media-file)))
          (wl-filenames (mapcar (lambda (f) (upcase (md5 f))) possible-filenames))
          (wl-files (mapcar (lambda (f) (file-name-concat mpvmedia-pos-watch-later-dir f)) wl-filenames))
          ;; Later we'll go through list and get last existing. If we prefer
-         ;; hashes with path, we should check it lately, because last successfull
+         ;; hashes with path, we should check it lately, because last successful
          ;; will be used
          (wl-files (if mpvmedia-pos-prefer-with-path (reverse wl-files) wl-files))
          selected-wl-file)
@@ -33,14 +39,29 @@
                             (buffer-string)))
          (wl-lines (string-split wl-file-content "\n"))
          (wl-alist (mapcar #'mpvmedia-pos--parse-wl-line wl-lines)))
-    (format-seconds
-     mpvmedia-pos-watched-time-format
-     (string-to-number (alist-get 'start wl-alist)))))
+    (string-to-number (alist-get 'start wl-alist))))
+
+(defun mpvmedia-pos--get-duration (media-file)
+  (when (executable-find mpvmedia-pos-mediainfo-command)
+    (/ (string-to-number
+        (shell-command-to-string
+         (string-join
+          `(,mpvmedia-pos-mediainfo-command
+            ,mpvmedia-pos-mediainfo-args
+            ,(shell-quote-argument media-file)) " "))) 1000.0)))
 
 (defun mpvmedia-pos-info-string (media-file)
-  (when-let* ((wl-file (mpvmedia-pos-watch-later-file media-file))
-              (mpvmedia-pos (mpvmedia-pos--extract-pos wl-file)))
-    (format mpvmedia-pos-format mpvmedia-pos)))
+  (when-let* ((wl-file (mpvmedia-pos--get-watch-later-file media-file)))
+
+    ;; current-pos duration completed format-str subst)
+    (let  ((current-pos (mpvmedia-pos--extract-pos wl-file))
+           (duration (mpvmedia-pos--get-duration media-file)))
+      (if duration
+          (if (>= (/ (float current-pos) duration) mpvmedia-pos-completed-threshold) ;completed if t
+              mpvmedia-pos-completed-message
+            (format mpvmedia-pos-format (round (* 100 (/ (float current-pos) duration)))))
+        (format mpvmedia-pos-fallback-format
+                (format-seconds mpvmedia-pos-watched-time-format current-pos))))))
 
 (provide 'mpvmedia-pos)
 ;;; mpvmedia-pos.el ends here
